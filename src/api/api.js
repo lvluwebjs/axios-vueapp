4d1bd0de0ef72ca6a741fa279aeb8a558ae0315c
lvlu_code <1406022840@qq.com>
2021-07-30 10:02:42 +0800

    修改注释

--- src/api/api.js
+++ src/api/api.js
@@ -5,14 +5,14 @@ import $ajax from '../request/request.js'
 /* 此文件用来统一管理项目所有接口函数 */
 //三种参数 params参数 query参数 请求体参数
 //params参数一般是在url路径中携带
-//query参数，可以在路径中携带，以？分割，后面就是query参数，也可以在配置对象中去配置（比如vuerouter路由）
+//query参数，可以在路径中携带，以？分割，后面就是query参数，也可以在配置对象中去配置
 //请求体参数 在配置对象data里面配置，一般请求方式是put和post才有请求体参数
 export const Login = (data) => {
   //登录接口，携带请求参数 data
   return $ajax({
       method:'post',//请求方式
       url:'/userlogin',//请求路径，会和axios实例中的 baseURL 进行拼接
-      data:qs.stringify(data),//data:{}为请求体参数
+      data:qs.stringify(data)//data:{}为请求体参数
       // params里面代表的是query参数
       // params:{}
      })

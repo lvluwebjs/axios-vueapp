7d3ea44c8235a506646c12e2527e3114cd34b793
lvlu_code <1406022840@qq.com>
2021-10-08 14:35:11 +0800

    tj

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
-const router = new Router({ 
-  mode:'history',
-  routes : [
-    {
-   // 设置为默认路由
-     path: '/',
-     redirect: '/login'
+const router = new Router({
+  mode: 'history',
+  routes: [{
+      // 设置为默认路由
+      path: '/',
+      redirect: '/login'
     },
     {
-     path:'/login',
-    //  使用ES中的import进行懒加载  (推荐使用，也是最常用的)
-     component:() => import('../components/Login/login.vue'),
-    //  使用VUE中的异步组件进行懒加载
-    // component: resolve => { require(['../components/Login/login.vue'],resovle); }
-    meta:{
-      requireAuth:true
+      path: '/login',
+      //  使用ES中的import进行懒加载  (推荐使用，也是最常用的)
+      component: () => import('../components/Login/login.vue'),
+      //  使用VUE中的异步组件进行懒加载
+      // component: resolve => { require(['../components/Login/login.vue'],resovle); }
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/home',
+      component: () => import('../views/Home/home.vue')
+    },
+    {
+      path: '/gwc',
+      component: () => import('../views/Gwc/gwc.vue')
     }
-   },
-    {
-     path:'/home',
-     component:() => import('../views/Home/home.vue')
-    },
-    {
-      path:'/gwc',
-      component:() => import('../views/Gwc/gwc.vue')
-     }
-   
-]
+
+  ]
 })
 // let norequireLogin = ['/login'];
 // router.beforeEach((to, form, next) => {
@@ -42,33 +41,37 @@ const router = new Router({
 //   }
 //   next();
 // })
-router.beforeEach((to, from ,next) => {
+router.beforeEach((to, from, next) => {
   const token = window.sessionStorage.getItem('tokens');
-  if(to.matched.some(record => record.meta.requireAuth)){
-      //路由元信息requireAuth:true，或者homePages:true，则不做登录校验
-      next()
-  }else{
-      if(token){//判断用户是否登录
-          if(Object.keys(from.query).length === 0){//判断路由来源是否有query，处理不是目的跳转的情况
-              next()
-          }else{
-              let redirect = from.query.redirect//如果来源路由有query
-              if(to.path === redirect){//这行是解决next无限循环的问题
-                  next()
-              }else{
-                  next({path:redirect})//跳转到目的路由
-              }
-          }
-      }else{
-          if(to.path==="/login"){
-              next()
-          }else{
-              next({
-                    path:"/login",
-                    query: {redirect: to.fullPath}//将目的路由地址存入login的query中
-              })
-          }
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    //路由元信息requireAuth:true，或者homePages:true，则不做登录校验
+    next()
+  } else {
+    if (token) { //判断用户是否登录
+      if (Object.keys(from.query).length === 0) { //判断路由来源是否有query，处理不是目的跳转的情况
+        next()
+      } else {
+        let redirect = from.query.redirect //如果来源路由有query
+        if (to.path === redirect) { //这行是解决next无限循环的问题
+          next()
+        } else {
+          next({
+            path: redirect
+          }) //跳转到目的路由
+        }
       }
+    } else {
+      if (to.path === "/login") {
+        next()
+      } else {
+        next({
+          path: "/login",
+          query: {
+            redirect: to.fullPath
+          } //将目的路由地址存入login的query中
+        })
+      }
+    }
   }
   return
 })
